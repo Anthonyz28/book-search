443ddd2074cc5e306614b749ad4238aa85aeacc3
Anthonyz28 <anthony.z28@hotmail.com>
2021-05-09 20:06:50 -0500

    heroku fix

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -31,7 +31,7 @@ const SignupForm = () => {
 
     try {
       const { data } = await ADD_USER({
-        variables: { ...formData }
+        variables: { ...userFormData }
       });
       Auth.login(data.addUser.token);
     } catch (e) {
